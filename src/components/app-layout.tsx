ef9bc79e44f8810e7ae0fde272bb65b28584459e
LIFE CARE MEDICALS 52 <lifecaremedicals52@gmail.com>
2025-07-13 12:41:55 +0000

    keep the notification button that is in between the other 2 buttons equa

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -135,7 +135,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
             </Tooltip>
             <Tooltip>
                 <TooltipTrigger asChild>
-                    <Button asChild variant="ghost" size="icon" className="relative mr-[-3px]">
+                    <Button asChild variant="ghost" size="icon" className="relative">
                         <Link href="/alerts">
                             <Bell />
                              {urgentAlertsCount > 0 && (
