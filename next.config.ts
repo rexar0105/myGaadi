60f8cd661fda7bae481fecf65c85c4e7f4bdce45
LIFE CARE MEDICALS 52 <lifecaremedicals52@gmail.com>
2025-07-14 12:03:53 +0000

    yes please

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,10 @@
 import type {NextConfig} from 'next';
+import withPWAInit from 'next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -20,4 +26,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
